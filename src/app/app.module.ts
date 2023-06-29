58ca1fb5387fab3cf6a180a89464188c4acabf12
vasudha27bajaj <vasudha.bajaj6799@gmail.com>
2023-06-29 10:37:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimeTrackerComponent } from './pages/time-tracker/time-tracker.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { TimeTrackerComponent } from './pages/time-tracker/time-tracker.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/time-tracker/time-tracker.component.ts
+++ src/app/pages/time-tracker/time-tracker.component.ts
@@ -1,23 +1,33 @@
 import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Tracker_List } from './time-tracker.model';
-import { MatDialog } from '@angular/material';
-
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
 @Component({
   selector: 'app-time-tracker',
   templateUrl: './time-tracker.component.html',
   styleUrls: ['./time-tracker.component.scss']
 })
 export class TimeTrackerComponent implements OnInit {
-  @ViewChild('addTask') addTask!: TemplateRef<any>;
+  @ViewChild('addTask') addTask = {} as TemplateRef<any>; 
   trackerList:Tracker_List[] = [{heading:'Adding animations to the website',
-  history:['No history found, click on the start button to track the timer']}];
+  history:[]}];
+  noHistoryFoundText = 'No history found, click on the start button to track the timer';
+  newTask = '';
   constructor(private matdialog:MatDialog) { }
 
   ngOnInit(): void {
   }
 
   openAddTaskPopup():void{
-    const dialog = this.matdialog.open(this.addTask);
+    const dialog = this.matdialog.open(this.addTask,{
+      minWidth:'25rem'
+    });
+    dialog.afterClosed().subscribe((res)=>{
+      if(res){
+        const taskToAdd:Tracker_List = {heading:this.newTask,history:[]};
+        this.trackerList.push(taskToAdd);
+        console.log(res);
+      }
+    })
   }
 
 }
